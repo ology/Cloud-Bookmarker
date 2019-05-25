ef64932a32a51e4e45a090c54fb68f625dda4d95
Gene Boggs <gene@ology.net>
2019-05-25 00:51:08 -0700

    Trap no item update error

--- lib/Bookmarker.pm
+++ lib/Bookmarker.pm
@@ -79,6 +79,8 @@ post '/update' => sub {
 
     send_error( UNKNOWN, 400 ) unless -e $file;
 
+    send_error( 'No item id provided', 400 ) unless $item;
+
     try {
         my @lines = _read_file($file);
 
