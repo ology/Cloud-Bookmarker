4a8f4149750b88fbb001c45dae3fd01e92dd1b7d
Gene Boggs <gene@ology.net>
2019-06-16 04:54:28 -0700

    Search case-insensitively

--- lib/Bookmarker.pm
+++ lib/Bookmarker.pm
@@ -82,7 +82,7 @@ post '/search' => sub {
 
             my ( $id, $title, $url, $tags ) = split /\t/, $line, 4;
 
-            if ( @query && any { $title =~ /\Q$_/ || $url =~ /\Q$_/ || $tags =~ /\Q$_/ } @query ) {
+            if ( @query && any { $title =~ /\Q$_/i || $url =~ /\Q$_/i || $tags =~ /\Q$_/i } @query ) {
                 push @$data, { id => $id, title => $title, url => $url, tags => $tags };
             }
         }
