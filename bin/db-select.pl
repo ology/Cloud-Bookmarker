ced4d20e9bc36474a61bf16eb842586ec8360b52
Gene Boggs <gene@ology.net>
2019-06-17 12:17:08 -0700

    Add users table select

--- bin/db-select.pl
+++ bin/db-select.pl
@@ -21,15 +21,26 @@ my $sql = 'SELECT * FROM bookmarks WHERE account = ? ORDER BY id';
 my $sth = $dbh->prepare($sql) or die $dbh->errstr;
 $sth->execute($account) or die $dbh->errstr;
 
-my %seen;
+#my %seen;
 
+print "Bookmarks:\n";
 while( my @row = $sth->fetchrow_array ) {
     print join( ',', @row ), "\n";
-    $seen{ $row[3] }++;
+#    $seen{ $row[3] }++;
+}
+
+$sql = 'SELECT * FROM users WHERE account = ? ORDER BY id';
+$sth = $dbh->prepare($sql) or die $dbh->errstr;
+$sth->execute($account) or die $dbh->errstr;
+
+print "Users:\n";
+while( my @row = $sth->fetchrow_array ) {
+    print join( ',', @row ), "\n";
 }
 
 $dbh->disconnect();
 
+__END__
 for my $k ( keys %seen ) {
     next if $seen{$k} <= 1;
     print "$k\n";
