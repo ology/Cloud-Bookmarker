d288fa110ac2799dfe71f3511ca397d7803c6233
Gene Boggs <gene@ology.net>
2019-06-17 12:14:37 -0700

    Add users insert

--- bin/db-insert.pl
+++ bin/db-insert.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 
+use Crypt::SaltedHash;
 use DBI;
 
 use constant PATH     => 'public/accounts/';
@@ -10,9 +11,10 @@ use constant ENCODING => ':encoding(UTF-8)';
 use constant NOAUTH   => 'Not authorized';
 use constant UNKNOWN  => 'Unknown account';
 
-my $account = shift or die "Usage: perl $0 123\n";
+my $account = shift or die "Usage: perl $0 account [passphrase]\n";
+my $pass    = shift;
 
-my $file = _auth($account);
+#my $file = _auth($account);
 
 my $driver   = 'SQLite';
 my $database = 'cloudbookmarker.db';
@@ -21,16 +23,27 @@ my $userid   = '';
 my $password = '';
 my $dbh      = DBI->connect( $dsn, $userid, $password, { RaiseError => 1 } ) or die $DBI::errstr;
 
-my $sql = 'INSERT INTO bookmarks (id, account, title, url, tags) VALUES (?, ?, ?, ?, ?)';
-my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+my ( $sql, $sth );
 
-open my $fh, '<' . ENCODING, $file or die "Can't read $file: $!";
-while ( my $line = readline($fh) ) {
-    chomp $line;
-    my ( $id, $title, $url, $tags ) = split /\t/, $line, 4;
-    $sth->execute( $id, $account, $title, $url, $tags ) or die $dbh->errstr;
+if ( $pass ) {
+    my $csh = Crypt::SaltedHash->new( algorithm => 'SHA-1' );
+    $csh->add($pass);
+    $pass = $csh->generate;
+
+    $sql = 'INSERT INTO users (id, account, password) VALUES (?, ?, ?)';
+    $sth = $dbh->prepare($sql) or die $dbh->errstr;
+    $sth->execute( time(), $account, $pass ) or die $dbh->errstr;
 }
-close $fh or die "Can't close $file: $!";
+
+#$sql = 'INSERT INTO bookmarks (id, account, title, url, tags) VALUES (?, ?, ?, ?, ?)';
+#$sth = $dbh->prepare($sql) or die $dbh->errstr;
+#open my $fh, '<' . ENCODING, $file or die "Can't read $file: $!";
+#while ( my $line = readline($fh) ) {
+#    chomp $line;
+#    my ( $id, $title, $url, $tags ) = split /\t/, $line, 4;
+#    $sth->execute( $id, $account, $title, $url, $tags ) or die $dbh->errstr;
+#}
+#close $fh or die "Can't close $file: $!";
 
 $dbh->disconnect();
 
