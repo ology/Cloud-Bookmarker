bcd50d20d7ef09d518853166285f34f232f1e15d
Gene Boggs <gene@ology.net>
2019-06-17 20:59:44 -0700

    Handy

---
+++ bin/db-check.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use DBI;
+use HTTP::Simple qw/ getstore is_error /;
+
+my $account = shift or die "Usage: perl $0 account\n";
+
+my $driver   = 'SQLite';
+my $database = 'cloudbookmarker.db';
+my $dsn      = "DBI:$driver:dbname=$database";
+my $userid   = '';
+my $password = '';
+my $dbh      = DBI->connect( $dsn, $userid, $password, { RaiseError => 1 } ) or die $DBI::errstr;
+
+my $sql = 'SELECT * FROM bookmarks WHERE account = ? ORDER BY id';
+my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+$sth->execute($account) or die $dbh->errstr;
+
+while( my @row = $sth->fetchrow_array ) {
+    my $status = getstore( $row[3], '/tmp/junk.html' );
+    if ( is_error($status) ) {
+        print $row[3], "\n";
+    };
+}
