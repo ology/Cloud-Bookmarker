a817ba85e0a1c03af81f92f9611c2b57cd814cfa
Gene Boggs <gene@ology.net>
2019-08-25 02:44:44 -0700

    Add AUTHOR and COPYRIGHT sections

--- lib/Bookmarker.pm
+++ lib/Bookmarker.pm
@@ -372,4 +372,15 @@ L<Netscape::Bookmarks>
 
 L<Try::Tiny>
 
+=head1 AUTHOR
+
+Gene Boggs <gene@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+ 
+This software is copyright (c) 2019 by Gene Boggs.
+ 
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
 =cut
